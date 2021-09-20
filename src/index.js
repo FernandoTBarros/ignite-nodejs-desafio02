9b2f4ab354f0644ca9e9ed7a4210b118caa01e8c
Fernando T. Barros <fernando@deltasi.com.br>
2021-09-20 01:39:45 -0300

    Implementing middleware functions to pass all tests

--- src/index.js
+++ src/index.js
@@ -10,19 +10,57 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+	const { username } = request.headers;
+
+	const user = users.find(user => username == user.username);
+	if(user) {
+	  request.user = user;
+	  next();
+	}
+	else {
+	  response.status(404).json({error:`Username does not exists with username ${username}`});
+	}
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+	const { user } = request;
+	if(!user.pro && user.todos.length >= 10) {
+		return response.status(403).json({error: "User is in free plan and already created 10 TODOs"})
+	}
+	next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+	const { username } = request.headers;
+
+	const user = users.find(user => username == user.username);
+	if(!user) {
+		return response.status(404).json({error:`Username does not exists with username ${username}`});
+	}
+	const { id } = request.params;
+	if (!validate(id)) {
+		return response.status(400).json({error:'ID is not a valid uuid'})
+	}
+	const todo = user.todos.find(todo => todo.id == id);
+	if (!todo) {
+		return response.status(404).json({ error: `TODO does not exists with ID ${id}` })
+	}
+	request.user = user;
+	request.todo = todo;
+	next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+	const { id } = request.params;
+
+	const user = users.find(user => id == user.id);
+	if(user) {
+	  request.user = user;
+	  next();
+	}
+	else {
+	  response.status(404).json({error:`Username does not exists with ID ${id}`})
+	}
 }
 
 app.post('/users', (request, response) => {
